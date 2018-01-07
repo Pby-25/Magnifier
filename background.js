e838f17eb79f3f8943812988ff10713c7fea4a93
Kael Du <agains1595@gmail.com>
2018-01-07 18:30:46 -0500

    created functional bacground script, added jquery to package

--- background.js
+++ background.js
@@ -1,1 +1,38 @@
-chrome.runtime.onInstalled.addListener(function() {});
+// TODO
+// chrome.runtime.onInstalled.addListener(function() {});
+// Simplified browser action first
+
+var id = 100;
+
+// Listen for a click on browser action icon
+chrome.browserAction.onClicked.addListener(function() {
+
+    chrome.tabs.captureVisibleTab(function(screenshotUrl) {
+        var viewTabUrl = chrome.extension.getURL('snapshot.html?id=' + id++)
+        var targetId = null;
+
+        chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps) {
+            // Wait for the tab we opened to finish loading.
+            if (tabId != targetId || changedProps.status != "complete")
+                return;
+            chrome.tabs.onUpdated.removeListener(listener);
+
+            // Look through all views to find the window which will display
+            // the screenshot.  The url of the tab which will display the
+            // screenshot includes a query parameter with a unique id, which
+            // ensures that exactly one view will have the matching URL.
+            var views = chrome.extension.getViews();
+            for (var i = 0; i < views.length; i++) {
+                var view = views[i];
+                if (view.location.href == viewTabUrl) {
+                    view.setScreenshotUrl(screenshotUrl);
+                    break;
+                }
+            }
+        });
+
+        chrome.tabs.create({url: viewTabUrl}, function(tab) {
+            targetId = tab.id;
+        });
+    });
+});
