4cda4683d2a9c2ec5b51e06b74208b506b791a98
Kael Du <agains1595@gmail.com>
2018-01-09 20:55:23 -0500

    The extension now sort of work in a barebone way
    To be debugged and optimized before adding features

--- magnifier.js
+++ magnifier.js
@@ -1,4 +1,47 @@
 function setScreenshotUrl(url) {
-    document.getElementById('target').src = url;
+    document.getElementById('top_layer').src = url;
+    document.getElementById("bottom_layer").style.background = "url('" + url + "') no-repeat";
 }
 
+$(function(){
+    var magnify_width = 0;
+    var magnify_height = 0;
+    var magnify_str = 2;
+
+    //Now the mousemove function
+    $(".magnify").mousemove(function(e){
+        if(!magnify_width && !magnify_height)
+        {
+            var img = document.getElementById("top_layer");
+            // TODO customizable magnification
+            magnify_width = img.width * magnify_str;
+            magnify_height = img.height * magnify_str;
+        }
+        else {
+            // Fade-in and fade-out the glass if the mouse is inside the page
+            if(e.pageX < $(this).width() && e.pageY < $(this).height() && e.pageX > 0 && e.pageY > 0)
+            {
+                $(".large").fadeIn(100);
+            }
+            else
+            {
+                $(".large").fadeOut(100);
+            }
+
+            if($(".large").is(":visible"))
+            {
+                // Calculate the relative position of large image
+                var x_offset = Math.round(e.pageX * magnify_str - $(".large").width()/2)*-1;
+                var y_offset = Math.round(e.pageY * magnify_str - $(".large").height()/2)*-1;
+                var bg_position = x_offset + "px " + y_offset + "px";
+
+                // Move the magnifying glass with the mouse
+                var x_position = e.pageX - $(".large").width()/2;
+                var y_position = e.pageY - $(".large").height()/2;
+
+                $(".large").css({left: x_position, top: y_position, backgroundPosition: bg_position});
+
+            };
+        };
+    });
+});
