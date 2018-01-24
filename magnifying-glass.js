fcb08377c2f587ab71e5269113914cd7dea4823b
Kael Du <agains1595@gmail.com>
2018-01-23 21:28:40 -0500

    fixed bugs related to cursor's position when page is scrolled

--- magnifying-glass.js
+++ magnifying-glass.js
@@ -23,9 +23,9 @@ chrome.runtime.onMessage.addListener(function transfer(config, sender){
         "0 0 " + 7/strength + "px " + 7/strength + "px rgba(0, 0, 0, 0.25), " +
         "inset 0 0 " + 40/strength + "px "+ 2/strength + "px rgba(0, 0, 0, 0.25)";
 
-    $('#_magnify_scope').mousemove(function(mm_event){
+    $('#_magnify_scope').mousemove(function(e){
         // Fade-in and fade-out the glass if the mouse is inside the page
-        if(mm_event.pageX < $(this).width()-1 && mm_event.pageY < $(this).height()-4 && mm_event.pageX > 0 && mm_event.pageY > 0)
+        if(e.clientX < $(this).width()-1 && e.clientY < $(this).height()-4 && e.clientX > 0 && e.clientY > 0)
         {
             $('#_bottom_layer').fadeIn(100);
         }
@@ -37,26 +37,29 @@ chrome.runtime.onMessage.addListener(function transfer(config, sender){
         if($('#_bottom_layer').is(':visible'))
         {
             // Calculate the relative position of large image
-            var x_offset = Math.round(mm_event.pageX - $('#_bottom_layer').width()/2)*-1;
-            var y_offset = Math.round(mm_event.pageY - $('#_bottom_layer').height()/2)*-1;
+            var x_offset = Math.round(e.clientX - $('#_bottom_layer').width()/2)*-1;
+            var y_offset = Math.round(e.clientY - $('#_bottom_layer').height()/2)*-1;
             var bg_position = x_offset + "px " + y_offset + "px";
 
             // Move the magnifying glass with the mouse
-            var x_position = mm_event.pageX - $('#_bottom_layer').width()/2;
-            var y_position = mm_event.pageY - $('#_bottom_layer').height()/2;
+            var x_position = e.clientX - $('#_bottom_layer').width()/2;
+            var y_position = e.clientY - $('#_bottom_layer').height()/2;
 
             $("#_bottom_layer").css({left: x_position, top: y_position, backgroundPosition: bg_position});
         };
     });
 
-    $('#_bottom_layer').keydown(function(kd_event){
+    $('#_bottom_layer').keydown(function(e){
         $('#_magnify_scope').remove();
     });
 
-    $('#_bottom_layer').mousedown(function(md_event){
+    $('#_bottom_layer').mousedown(function(e){
         $('#_magnify_scope').remove();
     });
 
-
+    $('#_bottom_layer').on('wheel', function(e){
+        $('#_magnify_scope').remove();
+    })
+    
 
 });
